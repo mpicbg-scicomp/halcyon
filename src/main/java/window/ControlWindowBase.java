83bcc7be5989232ab52e117b64f2d52185f76126
HongKee Moon <hkmoon@me.com>
2015-07-16 13:06:06 +0200

    Change ControlType to ControlWindowBase and ControlWindowInterface

---
+++ src/main/java/window/ControlWindowBase.java
@@ -0,0 +1,20 @@
+package window;
+
+import bibliothek.gui.dock.common.DefaultSingleCDockable;
+
+/**
+ * Basic Control type Window for tools, which will be overridden by inherited
+ * class
+ */
+public abstract class ControlWindowBase extends DefaultSingleCDockable implements ControlWindowInterface
+{
+	public ControlWindowBase( String name )
+	{
+		super( name );
+	}
+
+	@Override public String toString()
+	{
+		return this.getUniqueId();
+	}
+}
---
+++ src/main/java/window/ControlWindowInterface.java
@@ -0,0 +1,8 @@
+package window;
+
+/**
+ * ControlInterface
+ */
+public interface ControlWindowInterface
+{
+}
