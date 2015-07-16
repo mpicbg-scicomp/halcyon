973fbdef5e5f768d42cbb8ef55e99fb212842cd7
HongKee Moon <hkmoon@me.com>
2015-07-16 13:04:40 +0200

    Make ToolbarInterface and ToolbarBase

---
+++ src/main/java/window/ToolInterface.java
@@ -0,0 +1,8 @@
+package window;
+
+/**
+ * Toolbar Interface
+ */
+public interface ToolInterface
+{
+}
---
+++ src/main/java/window/ToolbarBase.java
@@ -0,0 +1,12 @@
+package window;
+
+/**
+ * Toolbar base class
+ */
+public abstract class ToolbarBase extends ControlWindowBase implements ToolInterface
+{
+	public ToolbarBase( String name )
+	{
+		super( name );
+	}
+}
