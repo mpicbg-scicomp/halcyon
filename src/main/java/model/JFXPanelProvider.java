381e5ae045a51dd2f6711391d2624159faed6854
HongKee Moon <hkmoon@me.com>
2015-07-16 13:02:00 +0200

    Support GUI backend in JFXPanel and JPanel

---
+++ src/main/java/model/JFXPanelProvider.java
@@ -0,0 +1,11 @@
+package model;
+
+import javafx.embed.swing.JFXPanel;
+
+/**
+ * JFXPanelProvider interface
+ */
+public interface JFXPanelProvider
+{
+	JFXPanel getJFXPanel();
+}
---
+++ src/main/java/model/JPanelProvider.java
@@ -0,0 +1,11 @@
+package model;
+
+import javax.swing.JPanel;
+
+/**
+ * JPanelProvider interface
+ */
+public interface JPanelProvider
+{
+	JPanel getJPanel();
+}
