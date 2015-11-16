b89923f536315486c8feb55675f06914743e8b29
HongKee Moon <hkmoon@me.com>
2015-11-16 10:05:24 +0100

    Change the version number for bintray

--- updateVersion.sh
+++ updateVersion.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-VERSION_NUMBER=1.0.0
+VERSION_NUMBER=0.1.1
 BUILD_DATE=$(date +"%d.%m.%Y %H:%M:%S")
 
 grep -v "^build.date" src/main/resources/Strings.properties > /tmp/Strings.properties
